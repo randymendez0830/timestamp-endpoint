fc6d4b3bd416ea40c7da35d244f102a8d1101caf
randymendez0830 <randymendez0830@gmail.com>
2025-11-21 01:22:08 -0500

    Rename variable for current time in handler

--- api/timestamp.js
+++ api/timestamp.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
-  const newYorkTime = new Date().toLocaleString("en-US", {
+  const now = new Date().toLocaleString("en-US", {
     timeZone: "America/New_York",
   });
 
-  res.status(200).json({ timestamp: newYorkTime });
+  res.status(200).json({ timestamp: now });
 }
