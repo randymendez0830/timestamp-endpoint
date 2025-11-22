8002ab2aba15383336771b3c147530ca75a98176
randymendez0830 <randymendez0830@gmail.com>
2025-11-21 22:25:29 -0500

    Implement CORS support in adas_schedule_update
    Added CORS headers to allow browser testing.

--- api/adas_schedule_update.js
+++ api/adas_schedule_update.js
@@ -1,6 +1,14 @@
-import { google } from "googleapis";
-
 export default async function handler(req, res) {
+
+  // Allow Vapi browser testing
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   try {
     if (req.method !== "POST") {
       return res.status(405).json({ error: "Method not allowed" });
